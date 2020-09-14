93a1f7021227e2d33c4e4aaad000b52b75f9433a
rampeyvr <rampeyvr@gmail.com>
2020-09-14 14:52:36 +0530

    my second commit

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ app.get('/', function(req, res){
 app.get('/home', function(req, res){
     res.send("this is my home page");
     
+var exp = require("express");
 });
 app.get('/about' ,function(req, res) {
     res.send("<HTML><body><h1>This is my home page</h1></body></HTML>");
     
 });
-var port = process.env.npm_package_config_port;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
     
   console.log("HTTP server is up on port number " + port);  
